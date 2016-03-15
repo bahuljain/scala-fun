afe9eb19547b635fd95684346679983d656b5c42
Bahul Jain <bkj2111@columbia.edu>
2016-03-15 19:15:31 -0400

    6.2

--- src/week6/Collections.scala
+++ src/week6/Collections.scala
@@ -8,6 +8,22 @@ object Collections extends App {
 		println(i)
 	}
 
-	val range: NumericRange[Double] = -5.0 to 5.0 by 0.5
-	println(range)
+	val nrange: NumericRange[Double] = -5.0 to 5.0 by 0.5
+	println(nrange)
+
+	val range = 1 to 10
+
+	println((1 to 10).toList flatMap (x => (1 to 10).toList map (y => (x, y))))
+
+	val vecX = Vector(1, 2, 3, 2, 1)
+	val vecY = Vector(1, 0, 1, 0, 1)
+
+	def scalarProd(vecX: Vector[Int], vecY: Vector[Int]): Int =
+		(vecX zip vecY).map(xy => xy._1 * xy._2).sum
+
+	println(scalarProd(vecX, vecY))
+
+	def isPrime_?(x: Int): Boolean = (2 until x) forall (d => x % d != 0)
+
+	assert(isPrime_?(29) == true)
 }
