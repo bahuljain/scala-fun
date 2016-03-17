76d2c8443ca210a57f4aa010cc49edad86f45139
Bahul Jain <bkj2111@columbia.edu>
2016-03-17 03:50:15 -0400

    clean and small bug fixes

--- src/leetcode/ValidAnagram.scala
+++ src/leetcode/ValidAnagram.scala
@@ -2,35 +2,27 @@ package leetcode
 
 object ValidAnagram extends App {
 	def isAnagram(s: String, t: String): Boolean = {
-
 		def aux(map: Map[Char, Int], indices: List[Int]): Boolean = indices match {
-			case Nil =>
-				if (map.forall(e => e._2 == 0)) true else false
-			case i :: is => {
+			case Nil => if (map.forall(e => e._2 == 0)) true else false
+			case i :: is =>
 				val tmp: Map[Char, Int] = map + (s(i) -> (map(s(i)) + 1))
 				aux(tmp.+(t(i) -> (tmp(t(i)) - 1)), is)
-			}
 		}
-
 		aux(Map() withDefaultValue 0, ((0 until s.length()).toList))
 	}
 
 	println(isAnagram("bahul", "luhab"))
 
 	def isAnagram_+(s: String, t: String): Boolean = {
-
-		def aux(indices: Range) = {
-			val map: Map[Char, Int] = Map() withDefaultValue 0
+		def aux(map: Map[Char, Int], indices: Range) =
 			indices.foldLeft(map) { (acc, i) =>
 				val tmp: Map[Char, Int] = acc + (s(i) -> (acc(s(i)) + 1))
 				tmp.+(t(i) -> (tmp(t(i)) - 1))
 			}
-		}
-
-		val res = aux(0 until s.length())
+		val res = aux(Map() withDefaultValue 0, 0 until s.length())
 		if (res.forall(e => e._2 == 0)) true else false
 	}
 
-	println(isAnagram("baahhul", "luehbah"))
+	println(isAnagram_+("baahhul", "luahbah"))
 
 }
