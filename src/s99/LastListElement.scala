63f5ac28c45990728a8e5bd64d9c6156256511ae
Bahul Jain <bkj2111@columbia.edu>
2016-03-22 11:47:06 -0400

    ..

--- src/s99/LastListElement.scala
+++ src/s99/LastListElement.scala
@@ -7,4 +7,12 @@ object LastListElement extends App {
 		list.last
 	}
 	println(last(List(1, 2, 3, 4, 5, 5, 6)))
+
+	def last1[T](list: List[T]): Option[T] = list match {
+		case Nil => None
+		case x :: Nil => Some(x)
+		case x :: xs => last1(xs)
+	}
+
+	println(last1(List(1, 2, 3, 4, 5, 5, 6)))
 }
