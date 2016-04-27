4646e1495b2c2fb7f3a3306cb0e8ddc798b5a100
Bahul Jain <bkj2111@columbia.edu>
2016-04-26 21:50:11 -0400

    summary ranges done finally

--- src/leetcode/SummaryRanges.scala
+++ src/leetcode/SummaryRanges.scala
@@ -1,8 +1,27 @@
 package leetcode
 
 object SummaryRanges extends App {
-	/*	def summary(arr: List[Int]): List[String] = {
-		def aux(arr: List[Int], prev: Int): List[(Int, Int)] = arr.fo
-	}*/
+	def summary(arr: List[Int]): List[String] = {
+		def stringify(start: Int, end: Int) =
+			if (start == end) s"$start"
+			else s"$start -> $end"
+
+		def aux(arr: List[Int], start: Int, prev: Int, acc: List[String]): List[String] = {
+			arr match {
+				case Nil => stringify(start, prev) :: acc
+				case x :: xs if (x == prev + 1) => aux(xs, start, x, acc)
+				case x :: xs => aux(xs, x, x, stringify(start, prev) :: acc)
+			}
+		}
+
+		if (arr.isEmpty) Nil
+		else aux(arr.tail, arr.head, arr.head, Nil) reverse
+	}
+
+	println(summary(List(1, 2, 3, 5, 6, 7, 9)))
+	println(summary(List(1)))
+	println(summary(List()))
+	println(summary(List(1, 3)))
+	println(summary(List(1, 2)))
 
 }
