fb7841a41b73d3ff934c6ea2d088a1a8b22d2d8e
Bahul Jain <bkj2111@columbia.edu>
2016-04-26 21:53:50 -0400

    adding test cases for isomorphic string

--- src/leetcode/IsomorphicStrings.scala
+++ src/leetcode/IsomorphicStrings.scala
@@ -9,12 +9,15 @@ object IsomorphicStrings extends App {
 				case Nil => true
 				case (c1, c2) :: lps =>
 					if (map.getOrElse(c1, c2) == c2 && rev.getOrElse(c2, c1) == c1)
-						aux(map.+(c1 -> c2), rev.+(c2 -> c1), lps)
+						aux(map + (c1 -> c2), rev + (c2 -> c1), lps)
 					else false
 			}
 		aux(Map(), Map(), (s1 zip s2).toList)
 	}
 
 	println(isomorphic("aab", "xyy"))
-
+	println(isomorphic("aay", "bbz"))
+	println(isomorphic("bahul", "japan"))
+	println(isomorphic("", "a"))
+	println(isomorphic("rubbish", "rubbers"))
 }
