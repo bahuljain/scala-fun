a667401f1dc63245f9a35272e8af724b3576ec10
Bahul Jain <bkj2111@columbia.edu>
2016-12-03 12:41:48 -0500

    using native transpose function

--- src/adventcode2016/Day3.scala
+++ src/adventcode2016/Day3.scala
@@ -4,11 +4,6 @@ object Day3 extends App {
 	def isValidTriangle(v: Array[Int]): Boolean =
 		v(0) + v(1) > v(2) && v(0) + v(2) > v(1) && v(1) + v(2) > v(0)
 
-	def transpose(group: List[List[Int]]): List[List[Int]] = group match {
-		case List(Nil, Nil, Nil) => Nil
-		case _ => group.map(_.head) :: transpose(group.map(_.tail))
-	}
-
 	val filename = "src/adventcode2016/day3_input.txt"
 	val lines = scala.io.Source.fromFile(filename).getLines.toList
 
@@ -19,7 +14,7 @@ object Day3 extends App {
 	val count2 = lines
 		.map(line => line.trim.split("""\s+""").toList.map(_.toInt))
 		.grouped(3).toList
-		.map(transpose).flatten
+		.map(_.transpose).flatten
 		.filter(v => isValidTriangle(v.toArray)).length
 
 	println(count1 + "\n" + count2)
