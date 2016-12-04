c10c647b577ffee9c31c62b4647a3f4ac07c35df
Bahul Jain <bkj2111@columbia.edu>
2016-12-04 05:39:37 -0500

    code sweetness

--- src/adventcode2016/Day4.scala
+++ src/adventcode2016/Day4.scala
@@ -1,28 +1,25 @@
 package adventcode2016
 
 object Day4 extends App {
-	def compare(a: (Char, Int), b: (Char, Int)): Boolean =
-		if (a._2 == b._2) a._1 < b._1 else a._2 > b._2
-
 	def decrypt(ch: Char, shift: Int): Char =
 		if (ch == '-') ' ' else ((ch - 'a' + shift) % 26 + 'a').toChar
 
 	val filename = "src/adventcode2016/day4_input.txt"
 	val lines = scala.io.Source.fromFile(filename).getLines.toList
+	val pattern = """([a-z-]+)(\d+)\[(\w+)\]""".r
 
-	val real = lines.map(l => l.splitAt(l.lastIndexOf('-'))) map { room =>
-		val tmp = room._2.tail.dropRight(1).split('[')
-		(room._1, tmp(0).toInt, tmp(1))
+	val real_rooms = lines.map(_.toLowerCase) map {
+		case pattern(name, id, chksm) => (name.dropRight(1), id.toInt, chksm)
 	} filter { room =>
 		room._1.filter(_ != '-')
 			.groupBy(identity).mapValues(_.length).toList
-			.sortWith(compare).take(5).map(_._1).mkString
-			.equals(room._3)
+			.sortBy(_._1).sortBy(-_._2).map(_._1).mkString
+			.startsWith(room._3)
 	}
 
-	val part1 = real map (_._2) sum
-	val part2 = (real find {
-		case (n, id, _) => n.map(decrypt(_, id)) == "northpole object storage"
+	val part1 = real_rooms map (_._2) sum
+	val part2 = (real_rooms find {
+		case (n, id, _) => n.map(decrypt(_, id)).startsWith("north")
 	}).get._2
 
 	println(part1 + "\n" + part2)
