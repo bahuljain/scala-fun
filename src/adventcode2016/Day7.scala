64018c3860f0ebc887d9877ed963acd205145b26
Bahul Jain <bkj2111@columbia.edu>
2017-01-08 17:05:30 +0530

    day 7 done

---
+++ src/adventcode2016/Day7.scala
@@ -0,0 +1,39 @@
+package adventcode2016
+
+object Day7 extends App {
+	val filename = "src/adventcode2016/day7_input.txt"
+	val lines = scala.io.Source.fromFile(filename).getLines.toList
+	val hypernet = """\[(\w+)\]""".r
+
+	def containsABBA(ip: String) = ip.sliding(4) exists { seq =>
+		seq(0) == seq(3) && seq(1) == seq(2) && seq(0) != seq(1)
+	}
+
+	val ips = lines.map(_.toLowerCase) map {
+		case ip =>
+			val hypernets = hypernet.findAllMatchIn(ip).map(_.toString.tail.dropRight(1))
+			val supernets = ip.split("""\[\w+\]""")
+			(hypernets toList, supernets toList)
+	}
+
+	val TLSSupport = ips count {
+		case (hypernets, supernets) =>
+			!hypernets.map(containsABBA).contains(true) &&
+				supernets.map(containsABBA).contains(true)
+	}
+
+	val SSLSupport = ips count {
+		case (hypernets, supernets) =>
+			supernets exists { supernet =>
+				supernet.sliding(3) exists { seq =>
+					seq(0) == seq(2) && seq(0) != seq(1) &&
+						hypernets.exists { hypernet =>
+							val bab = "" + seq(1) + seq(0) + seq(1)
+							hypernet.sliding(3).exists(_ == bab)
+						}
+				}
+			}
+	}
+
+	println(TLSSupport, SSLSupport)
+}
