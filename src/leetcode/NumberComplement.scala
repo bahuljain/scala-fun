6ac679651e5c1f97717e8568bbc304a690d28b13
Bahul Jain <bkj2111@columbia.edu>
2017-03-01 16:16:43 -0500

    Update NumberComplement.scala

--- src/leetcode/NumberComplement.scala
+++ src/leetcode/NumberComplement.scala
@@ -1,3 +1,5 @@
+package leetcode
+
 object NumberComplement extends App {
   def flip(c: Char): Char = if (c == '1') '0' else '1'
   def complement(x: Int): Int = Integer.parseInt(x.toBinaryString.map(flip), 2)
