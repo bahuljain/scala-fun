0a3bba2909daeed1418bd92ddbbc284e1ea66f38
Bahul Jain <bkj2111@columbia.edu>
2017-03-01 15:39:25 -0500

    Create DetectCapital.scala

---
+++ src/leetcode/DetectCapital.scala
@@ -0,0 +1,12 @@
+object DetectCapital extends App {
+  def isCapital(c: Char): Boolean = c >= 'A' && c <= 'Z'
+  def detectCapitalUse(word: String): Boolean = word.toList match {
+    case first :: rest if (isCapital(first)) => rest.forall(!isCapital(_)) || rest.forall(isCapital)
+    case word                                => word.forall(!isCapital(_))
+  }
+
+  println(detectCapitalUse("word"))
+  println(detectCapitalUse("USA"))
+  println(detectCapitalUse("Google"))
+  println(detectCapitalUse("FlaG"))
+}
