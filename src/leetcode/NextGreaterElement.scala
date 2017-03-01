525a14a229e2f035bcae373ce6137ab51430dc62
Bahul Jain <bkj2111@columbia.edu>
2017-03-01 15:42:49 -0500

    Create NextGreaterElement.scala

---
+++ src/leetcode/NextGreaterElement.scala
@@ -0,0 +1,7 @@
+object NextGreaterElement extends App {
+  def nextGreater(nums1: Array[Int], nums2: Array[Int]) = nums1 map { cur =>
+    nums2.dropWhile(_ != cur).find(_ > cur).getOrElse(-1)
+  } toList
+
+  println(nextGreater(Array(1, 2, 3), Array(1, 2, 3, 4)))
+}
