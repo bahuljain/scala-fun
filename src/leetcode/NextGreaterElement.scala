4a67c4084afdfb00d8685ac8213300835a192ee1
Bahul Jain <bkj2111@columbia.edu>
2017-03-01 16:16:22 -0500

    Update NextGreaterElement.scala

--- src/leetcode/NextGreaterElement.scala
+++ src/leetcode/NextGreaterElement.scala
@@ -1,3 +1,5 @@
+package leetcode
+
 object NextGreaterElement extends App {
   def nextGreater(nums1: Array[Int], nums2: Array[Int]) = nums1 map { cur =>
     nums2.dropWhile(_ != cur).find(_ > cur).getOrElse(-1)
